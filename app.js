72e3c8c61afc54dd09e947807ac48a78d344c137
Rajorshi <rajorshi.me@gmail.com>
2020-08-09 03:04:32 +0530

    initial commit. the script is fully functional

---
+++ app.js
@@ -0,0 +1,104 @@
+const puppeteer = require('puppeteer');
+const prompt = require('prompt-sync')({sigint: true});
+const {blizzardEmail, blizzardPassword} = require('./config');
+
+const getYoutubeLiveComments = async () => {
+    // const liveVideoUrl = 'https://www.youtube.com/watch?v=L8Djcwyvb_w';
+    const liveVideoUrl = prompt('Live video url: ').trim();
+
+
+    const browser = await puppeteer.launch({headless: true, defaultViewport: null});
+
+    // get cookies from blizzard battle.net
+    const blizzardCookies = await getBlizzardCookies(browser);
+
+    const page = await browser.newPage();
+    await page.goto(liveVideoUrl);
+    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) ' +
+        'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+        'Chrome/64.0.3282.39 Safari/537.36');
+
+    // switch to the chat iframe
+    await page.waitFor('iframe#chatframe')
+    console.log('iframe is ready. Loading iframe content');
+
+    // keep refreshing the messages
+    setInterval(async () => {
+        const elementHandle = await page.$('iframe#chatframe');
+        const chatFrame = await elementHandle.contentFrame();
+
+        let messages = await chatFrame.evaluate(() => {
+            return Array.from(document.querySelectorAll('span#message')).map(
+                message => message.innerText.trim()
+            )
+        });
+        console.log(messages)
+        for (const message of messages) {
+            let codeMatches = message.match(/(\w{2,10}-){3,10}(\w{2,10})/mg);
+            if (codeMatches !== null) {
+                const code = codeMatches[0];
+                console.log('======================== code found ========================');
+                console.log(code);
+                console.log('============================================================');
+                await redeemCode(blizzardCookies, code);
+            }
+        }
+        console.log(messages.length, 'messages found')
+    }, 200);
+};
+
+const getBlizzardCookies = async (browser) => {
+    console.log('logging into battle.net account')
+    const loginUrl = 'https://us.battle.net/login/en/?ref=https://us.battle.net/shop/en/checkout/key-claim&app=shop';
+    const email = blizzardEmail;
+    const password = blizzardPassword;
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) ' +
+        'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+        'Chrome/64.0.3282.39 Safari/537.36');
+    await page.goto(loginUrl);
+
+    await page.type('#accountName', email, {delay: 50});
+    await page.type('#password', password, {delay: 50});
+    await page.evaluate(() => {
+        document.querySelector('#submit').click();
+    })
+
+    // wait for next page to load
+    await page.waitFor('#enter-code-input')
+    console.log('logged in!')
+
+    // return the cookies
+    const cookies = (await page.cookies()).map(ck => `${ck["name"]}=${ck["value"]}`)
+    await page.close();
+    return cookies.join('; ');
+}
+
+const redeemCode = async (cookie, code) => {
+    console.log('redeeming code:', code);
+    const url = `https://us.battle.net/shop/en/checkout/key-claim?keyCode=${code}` +
+        '&returnUrl=https%3A%2F%2Fus.shop.battle.net%2F';
+
+    await fetch(url,
+        {
+            "headers": {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9," +
+                    "image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-language": "en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7",
+                "sec-fetch-dest": "document",
+                "sec-fetch-mode": "navigate",
+                "sec-fetch-site": "same-origin",
+                "sec-fetch-user": "?1",
+                "upgrade-insecure-requests": "1",
+                "cookie": cookie
+            },
+            "referrer": "https://us.battle.net/shop/en/checkout/key-claim",
+            "referrerPolicy": "no-referrer-when-downgrade",
+            "body": null,
+            "method": "GET",
+            "mode": "cors"
+        });
+    console.log('code redeemed!');
+}
+
+getYoutubeLiveComments();
